d815b21a05acac3271b7222393f31196dc66d40f
Maksim Kiselev <maksdears@gmail.com>
2018-11-26 15:08:22 +0300

    version 0.1

---
+++ app/footer.php
@@ -0,0 +1,75 @@
+    <footer class="container-fluid bg-my-grey">
+  <div class="container py-3">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-3 py-3 text-center">
+        <div><img src="images/logo-cik-white-text.png" width="170" height="50" class="rounded mx-auto d-block"></div>
+        <div class="mt-3 text-center">
+          <a href="" class="mx-1"><i class="fab fa-facebook-square fa-2x fa-color-white"></i></a>
+          <a href="" class="mx-1"><i class="fab fa-vk fa-2x fa-color-white"></i></a>
+          <a href="" class="mx-1"><i class="fab fa-youtube fa-2x fa-color-white"></i></a>
+          <a href="" class="mx-1"><i class="fab fa-instagram fa-2x fa-color-white"></i></a>
+        </div>
+        <a class="btn btn-outline-danger px-3 mt-4 text-white" href="#" role="button">Отправить заявку &raquo;</a>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-3 py-3 d-none d-lg-block">
+          <p class="text-secondary text-uppercase">сервисы</p>
+          <p><a href="" class="text-white none-decoration-a text-uppercase">ипотека</a></p>
+          <p><a href="" class="text-white none-decoration-a text-uppercase">жилые комплексы</a></p>
+          <p><a href="" class="text-white none-decoration-a text-uppercase">каталог компаний</a></p>
+          <p><a href="" class="text-white none-decoration-a text-uppercase">о компании</a></p>
+          <p><a href="" class="text-white none-decoration-a text-uppercase">контакты</a></p>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-3 py-3 d-none d-lg-block">
+
+<!--           <p>Полезная информация</p> -->
+          <p class="text-secondary text-uppercase">полезная информация</p>
+          <p><a href="" class="text-white none-decoration-a">Первичная консультация</a></p>
+          <p><a href="" class="text-white none-decoration-a">Рефинансирование</a></p>
+          <p><a href="" class="text-white none-decoration-a">Оплата услуг</a></p>
+          <p><a href="" class="text-white none-decoration-a">Юридическое сопровождение</a></p>
+          <p><a href="" class="text-white none-decoration-a">Подбор кредитования</a></p>
+
+       <!--  <ul class="text-white">
+          <li class="din-bold none-li-marker">Полезная информация</li>
+          <li class=""><a href="" class="none-decoration-a text-white">Первичная консультация</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Рефинансирование</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Оплата услуг</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Юридическое сопровождение</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Подбор кредитования</a></li>
+        </ul>         -->
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-3 py-3 d-none d-lg-block">
+
+<!--           <p>Полезная информация</p> -->
+          <p class="text-secondary text-uppercase">полезная информация</p>
+          <p><a href="" class="text-white none-decoration-a">Передача документов</a></p>
+          <p><a href="" class="text-white none-decoration-a">Переговоры с банками</a></p>
+          <p><a href="" class="text-white none-decoration-a">Сбор и оформление документов</a></p>
+          <p><a href="" class="text-white none-decoration-a">Профессиональные консультации</a></p>
+          <p><a href="" class="text-white none-decoration-a">Услуги по страхованию</a></p>
+
+        <!-- <ul class="text-white">
+          <li class="din-bold none-li-marker">Полезная информация</li>
+          <li class=""><a href="" class="none-decoration-a text-white">Передача документов</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Переговоры с банками</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Сбор и оформление документов</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Профессиональные консультации</a></li>
+          <li class=""><a href="" class="none-decoration-a text-white">Услуги по страхованию</a></li>
+        </ul>         -->
+      </div>
+    </div>
+      <p class="text-white text-center">&copy; Группа компаний "Центр ипотечного кредитования" 2006 - <?php echo date('Y')?></p>
+  </div>
+
+<!--Меню подвала-->
+    </footer>
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,188 @@
+<?php require_once('app/header.php');?>
+
+<!--Мини меню-->
+<div class="container-fluid bg-light d-none d-lg-block">
+  <div class="container">
+    <div class="row justify-content-end py-2">
+      <div class="col-sm-12 col-md-1 text-center"><a href="#" class="text-secondary none-decoration-a">Партнеры</a></div>
+      <div class="col-sm-12 col-md-2 text-center"><a href="#" class="text-secondary none-decoration-a">Вопрос ответ</a></div>
+      <div class="col-sm-12 col-md-1 text-center"><a href="#" class="text-secondary none-decoration-a">Сотрудники</a></div>
+    </div>
+</div>
+</div>
+<!--Мини меню-->
+<!--Шапка-->
+<div class="container pt-3 pb-3 justify-content-center d-none d-lg-block">
+  <div class="row align-items-center">
+    <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 py-1">
+      <a href="">
+        <img class="center-block" src="images/logo-cik.png" width="190" height="55" alt="Логотип">
+      </a>
+    </div>
+    <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3 d-none d-lg-block py-1">
+      <span class="text-uppercase font-weight-bold">Единое Окно</span><br><span> по приобретению<br> недвижимости</span>
+    </div>
+    <div class="col-sm-4 col-md-3 col-lg-2 col-xl-2 my-auto py-1">
+      <a href="tel:88007707041" class="text-secondary  font-weight-bold none-decoration-a din-bold text-dark">8 (800) 770 70 41</a><span class="text-secondary d-none d-lg-block"> (Бесплатно по РФ)</span>
+    </div>
+    <div class="col-sm-4 col-md-3 col-lg-2 col-xl-2 my-auto py-1">
+      <a href="tel:+74732067041" class="text-secondary font-weight-bold none-decoration-a din-bold text-dark">8 (473) 206 70 41</a>
+    </div>
+    <div class="col-sm-4 col-md-3 col-lg-2 col-xl-2 my-auto py-1">
+      <button type="button" class="btn btn-outline-danger">Оставить заявку</button>
+    </div>
+  </div>
+</div>
+<!--Шапка-->
+<!--Меню-->
+<div class="container-fluid bg-navs-top">
+  <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+     <div class="row">
+        <div class="col-6">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div class="col-6 justify-content-end">
+          <div class="d-xs-block d-sm-block d-md-block d-lg-none d-xl-none"><img class="ml-4" src="images/logo.png" width="90" height="40" alt="Логотип"></div>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mx-auto font-size-top-menu">
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Услуги <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Ипотека</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Недвижимость</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Переезд по РФ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Отзывы</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">О нас</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white mx-2" href="#">Контакты</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</div>
+<!--Меню-->
+<!-- jumbotron -->
+<div class="jumbotron">
+        <div class="container">
+            <h1 class="display-5 din-black text-uppercase">Ипотека любой сложности</h1>
+            <h4 class="din-bold text-5">на всей территории Российской Федерации</h4>
+            <div class="col-xs-12 col-sm-12 col-md-6">
+              <li class="red-li-marker mb-2">получение для Вас одобрения ипотеки с первой подачи в банк</li>
+              <li class="red-li-marker mb-2">возможно без первоначального взноса, после отказов в банках, а Ваша кредитная история не имеет для нас значения</li>
+              <li class="red-li-marker mb-2">спецпредложение - для "зарплатников" банков</li>
+            </div>
+            <p class="mt-4"><a class="btn btn-danger px-4" href="#" role="button">Перейти &raquo;</a></p>
+        </div>
+</div>
+
+
+
+
+
+
+
+
+<!--SEO text-->
+<div class="container-fluid bg-light py-4">
+  <div class="container">
+    <p class="h3 text-uppercase text-center din-bold ">мы надежные партнеры</p>
+    <p class="h6 text-center din-bold">готовые помочь на всех этапах сделки</p>
+
+<p><span class="text-primary">Ипотека - это продукт сложный,</span> поэтому приобретать квартиру под ипотеку лучше с профессионалами. «Центр ипотечного кредитования» работает по принципу «единого окна» по приобретению недвижимости в ипотеку и без. Наши специалисты владеют всеми нюансами и помогут Вам сэкономить Ваши время, нервы и деньги на всех этапах приобретения жилья. Например, <span class="text-primary">мы можем предоставить вам квартиры со скидками от застройщика, до 15%, плюс оптимальная ставка от банка, плюс экономия на оптимальных тарифах при страховании и оценке</span> – все это позволяет нам сэкономить Клиенту его деньги при приобретении жилья под ипотеку.</p>
+<p>Добавим, что мы сопровождаем Клиента на все этапах, вплоть до заселения:</p>
+<ul>
+  <li class="red-li-marker">Подбираем лучшую квартиру, исходя из пожеланий и ситуации каждого человека, с учетом партнерских скидок и акций от застройщиков; </li>
+  <li class="red-li-marker">Оформляем весь пакет необходимых документов;</li>
+  <li class="red-li-marker">Проводим переговоры со стороны заемщика: с застройщиком или продавцом, банком, страховой и оценочной компаниями. </li>
+</ul>
+
+
+<div class="border border-danger border-top-0 border-bottom-0 border-right-0 pl-4 py-2 bg-white">
+  <span class="text-primary">По поводу отказа в банке.</span> В каждом банке разные требования к заемщикам. Ипотечный брокер разбирается в каждой конкретной ситуации и подбирает самый оптимальный банк и программу, исходя из индивидуальной ситуации каждого человека, с учетом партнерских скидок от банка и действующих акций.
+</div>
+
+<p class="din-bold mt-2">В результате – получение одобрение ипотеки от банка, даже в «сложных случаях» за 11 лет работы более 4700 семей благодаря нам приобрели новое жилье под самые низкие проценты.</p>
+
+
+<p><span class="text-primary">По поводу ответственности на долгие годы.</span> Ипотека – это ответственность, но станет ли она для человека тяжким бременем или ключом от новой жизни, зависит от правильной расстановки сил! Только профессиональный ипотечный брокер может рассчитать оптимальный ежемесячный платеж, который будет финансово комфортным для потенциального заемщика. Выбирать, конечно, Вам, но мы видим в ипотеке только возможности!</p>
+  </div>
+</div>
+<!--SEO text-->
+
+<!--Нижний ФОС-->
+<div class="container-fluid bg-primary">
+  <div class="container p-5">
+    <p class="h3 text-white text-uppercase text-center din-bold">напишите нам</p>
+    <form action="#">
+      <div class="form-row">
+        <div class="col-md-4 mb-3">
+          <label for="validationServer01">Ваше имя:</label>
+          <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First name" value="Имя" required>
+<!--       <div class="valid-feedback">
+        Отлично!
+      </div>
+      <div class="invalid-feedback">
+         Пожалуйста, введите ваше имя!
+      </div> -->
+        </div>
+        <div class="col-md-4 mb-3">
+          <label for="validationServer02">Ваш телефон</label>
+          <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Last name" value="8 (9xx) xxx-xx-xx" required>
+  <!--       <div class="valid-feedback">
+           Отлично!
+        </div>
+        <div class="invalid-feedback">
+           Пожалуйста, введите ваш телефон!
+        </div> -->
+        </div>
+        <div class="col-md-4 mb-3">
+          <label for="validationServer02">Ваше сообщение</label>
+          <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Last name" value="Сообщение" required>
+<!--       <div class="valid-feedback">
+         Отлично!
+      </div>
+      <div class="invalid-feedback">
+         Пожалуйста, напишите сообщение!
+      </div> -->
+        </div>
+      </div>
+      <div class="row align-items-center justify-content-end">
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
+              <label class="form-check-label text-white" for="invalidCheck3">
+                Согласие на обработку персональных данных
+              </label>
+              <div class="invalid-feedback text-white">
+                Чтобы отправить, необходимо согласиться!
+              </div>
+            </div>
+          </div>
+        </div>
+         <div class="col-xs-12 col-sm-12 col-md-4 justify-content-end">
+          <p class="mt-4"><a class="btn btn-danger px-5" href="#" role="button">Отправить &raquo;</a></p>
+         </div>
+      </div>
+    </form>
+  </div>
+</div>
+<!--Нижний ФОС-->
+
+<?php require_once('app/footer.php');?>
