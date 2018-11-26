a5fb427d5ebb16a47c492cd4ee50f848830dcd9a
Maksim Kiselev <maksdears@gmail.com>
2018-11-26 16:59:17 +0300

    version 0.2.2 end day

--- index.php
+++ index.php
@@ -5,9 +5,126 @@
 <?php require_once('app/jumbotron.php');?>
 <?php require_once('app/uslugi.php');?>
 
+<!--TODO-->
+1. Актуальные ипотечные программы
+  Сделать фоны
+2. Наименование блоков
+  Выделить, увеличить отступы сверху и снизу, должны выделяться
+3. Блок "Наши услуги"
+  С col-md- необходимо появление картинки / иконки
+4. Наши преимущества сделать
+5. Сделать блок отзывы для главной
+6. Создать открывание окон при нажатии оставить заявку
+7. Блок мы группа компаний
 
 
 
+<!--TODO-->
+
+
+
+
+
+
+<div class="container-fluid bg-light">
+  <p class="h3 text-uppercase text-center din-bold pt-4">актуальные ипотечные программы</p>
+<!--Длинный блок процентных ставок-->
+  <div class="container">
+      <div class="mt-3 px-3 bg-white">
+        <div class="row align-items-center py-5">
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 my-2">
+            <img src="images/dk-logo.png" alt="Сбербанк" width="200" height="55">
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 my-2">
+            <p class="h4 din-bold text-center">от 9,8%</p>
+            <p class="text-secondary text-center">Ставка в год</p>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 my-2">
+            <p class="h4 din-bold text-center">от 10%</p>
+            <p class="text-secondary text-center">Первоначальный взнос</p>
+          </div>
+          <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 my-2">
+            <p class="h4 din-bold text-center">25 лет</p>
+            <p class="text-secondary text-center">Срок кредита</p>
+          </div>
+        </div>
+      </div><hr>
+<!--Длинный блок процентных ставок-->
+<!--Короткие блоки процентных ставок-->
+
+
+    <div class="px-3 bg-white">
+      <div class="row">
+        <!--Первый блок-->
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="my-2">
+            <img class="rounded mx-auto d-block" src="images/dk-logo.png" alt="Сбербанк" width="160" height="45">
+          </div>
+          <div class="my-2">
+            <p class="h5 din-bold text-center">от 9,8%</p>
+            <p class="text-secondary text-center">Ставка в год</p>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <p class="h5 din-bold text-center">от 10%</p>
+              <p class="text-secondary text-center">Первоначальный взнос</p>
+            </div>
+            <div class="col-6">
+              <p class="h5 din-bold text-center">25 лет</p>
+              <p class="text-secondary text-center">Срок кредита</p>
+            </div>
+          </div>
+        </div>
+<!--Первый блок-->
+        <!--Первый блок-->
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="my-2">
+            <img class="rounded mx-auto d-block" src="images/dk-logo.png" alt="Сбербанк" width="160" height="45">
+          </div>
+          <div class="my-2">
+            <p class="h5 din-bold text-center">от 9,8%</p>
+            <p class="text-secondary text-center">Ставка в год</p>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <p class="h5 din-bold text-center">от 10%</p>
+              <p class="text-secondary text-center">Первоначальный взнос</p>
+            </div>
+            <div class="col-6">
+              <p class="h5 din-bold text-center">25 лет</p>
+              <p class="text-secondary text-center">Срок кредита</p>
+            </div>
+          </div>
+        </div>
+<!--Первый блок-->
+        <!--Первый блок-->
+        <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="my-2">
+            <img class="rounded mx-auto d-block" src="images/dk-logo.png" alt="Сбербанк" width="160" height="45">
+          </div>
+          <div class="my-2">
+            <p class="h5 din-bold text-center">от 9,8%</p>
+            <p class="text-secondary text-center">Ставка в год</p>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <p class="h5 din-bold text-center">от 10%</p>
+              <p class="text-secondary text-center">Первоначальный взнос</p>
+            </div>
+            <div class="col-6">
+              <p class="h5 din-bold text-center">25 лет</p>
+              <p class="text-secondary text-center">Срок кредита</p>
+            </div>
+          </div>
+        </div>
+<!--Первый блок-->
+      </div>
+    </div>
+
+
+  </div>
+
+</div>
 
 
 
