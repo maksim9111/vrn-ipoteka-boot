0c03d8a3ff499ef46b311baa1c65a37ef5e5d2bf
Maksim Kiselev <maksdears@gmail.com>
2018-11-26 16:20:30 +0300

    version 0.2

--- index.php
+++ index.php
@@ -6,6 +6,136 @@
 
 
 
+<!--Наши услуги-->
+<div class="container-fluid bg-light">
+  <p class="h3 text-uppercase text-center din-bold pt-3">наши услуги</p>
+  <p class="h6 text-center din-bold">Предоставляем любую консультацию по недвижимости - <span class="text-danger text-uppercase">бесплатно!</span></p>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-1.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50 d-none d-lg-block">
+            <h5 class="mt-0 text-uppercase din-medium">ипотечный центр</h5>
+            <p>Наши брокеры снизят Вам процентную ставку,  окажут помощь в подборе банка, ипотечной программы!</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+<!--Изменение вида блока-->
+            <div class="media-body px-3 d-xs-block d-sm-block d-md-block d-lg-none d-xl-none">
+              <h5 class="mt-0 text-uppercase din-medium">ипотечный центр</h5>
+              <p>Наши брокеры снизят Вам процентную ставку,  окажут помощь в подборе банка, ипотечной программы!</p>
+              <p>Наша комиссия: от 19 000 руб.</p>
+              <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+            </div>
+<!--Изменение вида блока-->
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 d-none d-lg-block">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-2.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50">
+            <h5 class="mt-0 text-uppercase din-medium">новостройки без комиссии</h5>
+            <p>Все жилые комплексы от парнеров-застройщиков, актуальные цены, скидки и акции!</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--=====================================================================-->
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-3.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50 d-none d-lg-block">
+            <h5 class="mt-0 text-uppercase din-medium">купля продажа недвижимости</h5>
+            <p>Наши специалисты по продаже недвижимости выгодно продадут и выгодно приобретут для Вас дом, квартиру, офис.</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+<!--Изменение вида блока-->
+            <div class="media-body px-3 d-xs-block d-sm-block d-md-block d-lg-none d-xl-none">
+              <h5 class="mt-0 text-uppercase din-medium">купля продажа недвижимости</h5>
+              <p>Наши специалисты по продаже недвижимости выгодно продадут и выгодно приобретут для Вас дом, квартиру, офис.</p>
+              <p>Наша комиссия: от 19 000 руб.</p>
+              <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+            </div>
+<!--Изменение вида блока-->
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 d-none d-lg-block">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-4.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50">
+            <h5 class="mt-0 text-uppercase din-medium">юридические услуги</h5>
+            <p>Четко и грамотно окажем юридическое сопровождение сделок с недвижимостью</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--=====================================================================-->
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-5.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50 d-none d-lg-block">
+            <h5 class="mt-0 text-uppercase din-medium">переезд в воронеж</h5>
+            <p>Обеспечим комфортный и быстрый переезд, поможем с продажей покупкой, ипотекой, организуем перевоз вещей.</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+<!--Изменение вида блока-->
+            <div class="media-body px-3 d-xs-block d-sm-block d-md-block d-lg-none d-xl-none">
+              <h5 class="mt-0 text-uppercase din-medium">переезд в воронеж</h5>
+              <p>Обеспечим комфортный и быстрый переезд, поможем с продажей покупкой, ипотекой, организуем перевоз вещей.</p>
+              <p>Наша комиссия: от 19 000 руб.</p>
+              <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+            </div>
+<!--Изменение вида блока-->
+         </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 d-none d-lg-block">
+        <div class="media bg-white py-3">
+          <div class="position-absolute img-top-left-15 d-none d-lg-block"><img class=" mr-3" src="images/forma-6.png" alt="Ипотечная услуга"></div>
+          <div class="media-body padding-left-50 d-none d-lg-block">
+            <h5 class="mt-0 text-uppercase din-medium">дополнительный сервис</h5>
+            <p>Предоставим скидки от наших компаний-парнеров на: ремонт, клининг.</p>
+            <p>Наша комиссия: от 19 000 руб.</p>
+            <a class="text-left text-danger none-decoration-a" href="#">Подробнее &raquo;</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+<!--Наши услуги-->
+
+
+
+
+
+
 
 
 
