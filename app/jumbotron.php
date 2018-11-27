f1ee0b26bd7ee8f479ca7c9da9939c9198b6a9ca
Maksim Kiselev <maksdears@gmail.com>
2018-11-27 12:47:45 +0300

    version 1.3 stabe

--- app/jumbotron.php
+++ app/jumbotron.php
@@ -1,24 +1,81 @@
 <!-- TODO
-- Некорректное отображение девочки, появляются черные границы, сама картинка обрезана
-- Текст в списке, подумать над нормальным отображением, чего то не хватает
-- 
+
 
 
 -->
-<!-- jumbotron -->
-<div class="jumbotron">
-        <div class="container">
-            <h1 class="display-5 din-black text-uppercase">Ипотека любой сложности</h1>
-            <h4 class="din-bold text-5">на всей территории Российской Федерации</h4>
-            <div class="col-xs-12 col-sm-12 col-md-6">
-              <li class="red-li-marker mb-2">получение для Вас одобрения ипотеки с первой подачи в банк</li>
-              <li class="red-li-marker mb-2">возможно без первоначального взноса, после отказов в банках, а Ваша кредитная история не имеет для нас значения</li>
-              <li class="red-li-marker mb-2">спецпредложение - для "зарплатников" банков</li>
+
+<div id="carouselExampleControls" class="carousel slide d-none d-lg-block" data-ride="carousel">
+  <div class="carousel-inner">
+    <!--ПЕРВЫЙ СЛАЙД-->
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../images/slider/slide1.png?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+      <div class="carousel-caption text-dark">
+        <div class="container fixed-top mt-100">
+          <div class="row">
+            <div class="col-md-9 text-left">
+              <h1 class="display-5 din-black text-uppercase">Ипотека любой сложности</h1>
+                  <h4 class="din-bold text-5">на всей территории Российской Федерации</h4>
+                  <div class="col-xs-12 col-sm-12 col-md-10 d-none d-xl-block">
+                    <li class="red-li-marker mb-2">получение для Вас одобрения ипотеки с первой подачи в банк</li>
+                    <li class="red-li-marker mb-2">возможно без первоначального взноса, после отказов в банках, а Ваша кредитная история не имеет для нас значения</li>
+                    <li class="red-li-marker mb-2">спецпредложение - для "зарплатников" банков</li>
+                  </div>
+                  <p class="mt-4"><a class="btn btn-danger px-4" href="#" role="button">Подробнее &raquo;</a></p>
             </div>
-            <p class="mt-4"><a class="btn btn-danger px-4" href="#" role="button"  data-toggle="modal" data-target="#fosTop">Купить недвижимость &raquo;</a></p>
+          </div>
         </div>
+      </div>
+    </div>
+    <!--ПЕРВЫЙ СЛАЙД-->
+    <!--ВТОРОЙ СЛАЙД-->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../images/slider/slide2.png?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+      <div class="carousel-caption text-dark">
+        <div class="container fixed-top mt-100">
+          <div class="row">
+            <div class="col-md-10 text-left">
+              <h1 class="display-5 din-black text-uppercase">Купить или продать недвижимость</h1>
+                  <h4 class="din-bold text-5">на всей территории Российской Федерации</h4>
+                  <div class="col-xs-12 col-sm-12 col-md-8 d-none d-xl-block">
+                    <li class="red-li-marker mb-2">получение для Вас одобрения ипотеки с первой подачи в банк</li>
+                    <li class="red-li-marker mb-2">возможно без первоначального взноса, после отказов в банках, а Ваша кредитная история не имеет для нас значения</li>
+                    <li class="red-li-marker mb-2">спецпредложение - для "зарплатников" банков</li>
+                  </div>
+                  <p class="mt-4"><a class="btn btn-danger px-4" href="#" role="button">Подробнее &raquo;</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--ВТОРОЙ СЛАЙД-->
+    <!--ТРЕТИЙ СЛАЙД-->
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="../images/slider/slide3.png?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+      <div class="carousel-caption text-dark">
+        <div class="container fixed-top mt-100">
+          <div class="row">
+            <div class="col-md-8 text-left">
+              <h1 class="display-5 din-black text-uppercase">Переезд в Воронеж</h1>
+                  <h4 class="din-bold text-5">из любой точки планеты</h4>
+                  <div class="col-xs-12 col-sm-12 col-md-10 d-none d-xl-block">
+                    <li class="red-li-marker mb-2">получение для Вас одобрения ипотеки с первой подачи в банк</li>
+                    <li class="red-li-marker mb-2">возможно без первоначального взноса, после отказов в банках, а Ваша кредитная история не имеет для нас значения</li>
+                    <li class="red-li-marker mb-2">спецпредложение - для "зарплатников" банков</li>
+                  </div>
+                  <p class="mt-4"><a class="btn btn-danger px-4" href="#" role="button">Подробнее &raquo;</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!--ТРЕТИЙ СЛАЙД-->
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
-<?php
-require_once('obratcall.php');
-require_once('persdannie.php');
-?>
